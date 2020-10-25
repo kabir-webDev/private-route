edb0950f2c743ac5e396f25835327b8b42194f0d
kabir-webDev <kabir.webdev@gmail.com>
2020-10-25 11:47:00 +0600

    Updated context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ShowCompo from './ShowCompo';
 import Compo from './Compo';
+import Condi from './Condi';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
   return (
     <div className="App">
-      <h2>Here is Info:</h2>
+      {/* <h2>Here is Info:</h2>
       <Compo/>
       <ShowCompo/>
-    
+     */}
+     <Condi isAuth={isAuth} setIsAuth={setIsAuth}>
+
+     </Condi>
     </div>
   );
 }
+
 
 export default App;
